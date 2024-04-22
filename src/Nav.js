64089becb7c138acc4c6879f3909b2f8919971e7
orabadanux <orabadanux@gmail.com>
2024-04-22 18:59:26 +0200

    Added Header, Nav, Main and Footer Components and meta tags

---
+++ src/Nav.js
@@ -0,0 +1,13 @@
+function Nav(){
+    return (
+        <nav>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/menu">Menu</a></li>
+          <li><a href="/reserve">Reserve a Table</a></li>
+          <li><a href="/contact">Contact</a></li>
+        </ul>
+      </nav>
+    );
+}
+export default Nav;
